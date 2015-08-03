f9d78f84b8cde18468d38eca6fc4f1ed7f60200b
samuel <samuelololol@gmail.com>
2015-08-04 01:02:17 +0800

    prepare time info as post request string

--- testcurl/test/post.c
+++ testcurl/test/post.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <curl/curl.h>
 #include <string.h>
+#include <time.h>
 #include "restful.h"
 
 const char* url = "http://localhost:6543/tachikoma";
@@ -12,14 +14,29 @@ int main(){
     struct RESTFUL_STRING *response;
     RESTFUL_STRING_INIT(&response);
 
-    char* request = "this is requests";
+    //char* request = "this is requests";
+    time_t current;
+    current = time(NULL);
+    if (current == ((time_t)-1))
+    {
+        (void) fprintf(stderr, "Failure to compute the current time.\n");
+        return EXIT_FAILURE;
+    }
+    char* c_time_string;
+    c_time_string = ctime(&current);
+    if (c_time_string == NULL)
+    {
+        (void) fprintf(stderr, "Failure to convert the current time.\n");
+        return EXIT_FAILURE;
+    }
+    printf("main: c_time_string: %s\n", c_time_string);
 
 
     // init curl
     RESTFUL_INIT(&curl);
 
     // normal processing
-    RESTFUL_POST(&curl, url, (void *) request, (void *)response);
+    RESTFUL_POST(&curl, url, (void *) c_time_string, (void *)response);
 
     printf("response:\n%s", response->ptr);
     RESTFUL_STRING_DESTROY(&response);
